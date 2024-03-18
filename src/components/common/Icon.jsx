4d6b68165c0a067b5b75eaa321c5018d4e348fc6
Hank Chen <cchen9299@gmail.com>
2024-03-18 13:42:42 -0700

    feat(icons): update icon sets

--- src/components/common/Icon.jsx
+++ src/components/common/Icon.jsx
@@ -1,20 +1,18 @@
 import {string} from 'prop-types';
 
 import React from 'react';
-import Balloon from '../../icons/Balloon.svg';
-import Cactus from '../../icons/Cactus.svg';
-import Cloud from '../../icons/Cloud.svg';
-import Eye from '../../icons/Eye.svg';
-import FlowerLotus from '../../icons/FlowerLotus.svg';
-import FlyingSaucer from '../../icons/FlyingSaucer.svg';
-import Ghost from '../../icons/Ghost.svg';
-import Infinity from '../../icons/Infinity.svg';
-import PinWheel from '../../icons/PinWheel.svg';
-import Planet from '../../icons/Planet.svg';
-import Robot from '../../icons/Robot.svg';
-import RocketLaunch from '../../icons/RocketLaunch.svg';
-import Skull from '../../icons/Skull.svg';
-import YinYang from '../../icons/YinYang.svg';
+import Balloon from '../../icons/balloon.svg';
+import Cactus from '../../icons/cactus.svg';
+import Cloud from '../../icons/cloud.svg';
+import Clover from '../../icons/clover.svg';
+import Cycle from '../../icons/cycle.svg';
+import Ghost from '../../icons/ghost.svg';
+import Planet from '../../icons/planet.svg';
+import YinYang from '../../icons/yinyang.svg';
+import Lily from '../../icons/lily.svg';
+import Palm from '../../icons/palm.svg';
+import Star from '../../icons/star.svg';
+import Ufo from '../../icons/ufo.svg';
 
 
 /**
@@ -24,20 +22,18 @@ import YinYang from '../../icons/YinYang.svg';
  */
 export default function Icon({name, ...rest}) {
   const icons = {
-    Balloon: Balloon,
-    Cactus: Cactus,
-    Cloud: Cloud,
-    Eye: Eye,
-    FlowerLotus: FlowerLotus,
-    FlyingSaucer: FlyingSaucer,
-    Ghost: Ghost,
-    Infinity: Infinity,
-    PinWheel: PinWheel,
-    Planet: Planet,
-    Robot: Robot,
-    RocketLaunch: RocketLaunch,
-    Skull: Skull,
-    YinYang: YinYang,
+    Balloon,
+    Cactus,
+    Cloud,
+    Clover,
+    Cycle,
+    Ghost,
+    Planet,
+    YinYang,
+    Lily,
+    Palm,
+    Star,
+    Ufo,
   };
   return <img src={icons[name]} {...rest}/>;
 }
