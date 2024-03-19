8fd7295db68fb54a1eba5f044ee98dcc10540944
Hank Chen <cchen9299@gmail.com>
2024-03-18 17:40:53 -0700

    feat(home): add random video selection

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,15 @@
 import React,
-{useRef, useState} from 'react';
+{useEffect, useRef, useState} from 'react';
 import cloud1 from '../video/cloud_1.mp4';
+import cloud2 from '../video/cloud_2.mp4';
 
 /**
  * @return {Node} home landing page
  */
 export default function Home() {
-  const [isMuted, setIsMuted] = useState(true);
+  // const [isMuted, setIsMuted] = useState(true);
   const [isPaused, setIsPaused] = useState(false);
+  const [video, setVideo] = useState(null);
   const videoRef = useRef();
 
   const playPause = () => {
@@ -20,6 +22,23 @@ export default function Home() {
     }
   };
 
+  const selectVideo = () => {
+    const videoMap = {
+      0: cloud1,
+      1: cloud2,
+      2: cloud2,
+      3: cloud2,
+    };
+    const num = Math.floor(Math.random() * 3);
+    setVideo(videoMap[num]);
+  };
+
+  useEffect(()=>{
+    selectVideo();
+  }, []);
+
+  if (!video) return null;
+
   return (
     <div
       className="full-view"
@@ -31,11 +50,12 @@ export default function Home() {
         controls={false}
         className="full-view"
         style={{objectFit: 'cover'}}
-        muted={isMuted}
         ref={videoRef}
         loop={true}
+        muted={true}
+        // muted={isMuted}
       >
-        <source src={cloud1} type="video/mp4" />
+        <source src={video} type="video/mp4" />
       </video>
       <div style={{
         zIndex: 1000,
@@ -45,7 +65,7 @@ export default function Home() {
 
       }}
       >
-        <button
+        {/* <button
           onClick={()=>setIsMuted(!isMuted)}
           style={{padding: 10}}
         >
@@ -53,7 +73,7 @@ export default function Home() {
             title={isMuted ? 'click to unmute' : 'click to mute'}
             className={`fa-lg fa-solid fa-volume-${isMuted ? 'xmark' : 'high'}`}
           />
-        </button>
+        </button> */}
         <button
           onClick={playPause}
           style={{padding: 10}}
